188f73c9cf3840a41e35491fee20392509c7a4bf
allan <allanmathenge2@gmail.com>
2023-06-12 16:44:09 +0300

    Mobile response

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -31,7 +31,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width:500px)');
+    const mediaQuery = window.matchMedia('(max-width:450px)');
 
     setIsMobile(mediaQuery.matches);
 
